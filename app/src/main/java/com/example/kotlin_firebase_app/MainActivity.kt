055225cadcc20066e195774d72d487d4334ad689
HansakaHeli <programminghhr66@gmail.com>
2024-02-27 11:48:14 +0530

    Read data from firebase

--- app/src/main/java/com/example/kotlin_firebase_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_firebase_app/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.kotlin_firebase_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +18,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView:TextView = findViewById(R.id.textView)
+
         // Realtime Database reference
         //https://fir-kotlin-b3165-default-rtdb.firebaseio.com
         database = Firebase.database.reference
 
         // Write data to Firebase
         database.child("price").setValue("1999 $")
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val gold_price = snapshot.value
+                textView.text = gold_price.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        database.child("price").addValueEventListener(postListener)
 
     }
 }
